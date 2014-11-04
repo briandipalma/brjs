dee26aebcc1fa54627e636d507975cc612be403d
Ioana Balas <ioana.balas@caplin.com>
2014-11-04 14:50:47 +0000

    added superclass Workbench for all workbenches
    Conflicts:
    	brjs-core/src/main/java/org/bladerunnerjs/model/BladeWorkbench.java

--- brjs-core/src/main/java/org/bladerunnerjs/model/BladeWorkbench.java
+++ brjs-core/src/main/java/org/bladerunnerjs/model/BladeWorkbench.java
@@ -1,67 +1,23 @@
 package org.bladerunnerjs.model;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 import org.bladerunnerjs.memoization.MemoizedFile;
 import org.bladerunnerjs.model.engine.Node;
-import org.bladerunnerjs.model.engine.NodeItem;
-import org.bladerunnerjs.model.engine.NodeList;
 import org.bladerunnerjs.model.engine.RootNode;
-import org.bladerunnerjs.model.exception.modelupdate.ModelUpdateException;
-import org.bladerunnerjs.plugin.utility.IndexPageSeedLocator;
-import org.bladerunnerjs.utility.TestRunner;
 
-public final class BladeWorkbench extends AbstractBrowsableNode implements TestableNode
+public final class BladeWorkbench extends Workbench
 {
-	private final NodeItem<DirNode> styleResources = new NodeItem<>(this, DirNode.class, "resources/style");
-	private final NodeList<TypedTestPack> testTypes = TypedTestPack.createNodeSet(this, TypedTestPack.class);
-	private final IndexPageSeedLocator seedLocator;
 	
 	public BladeWorkbench(RootNode rootNode, Node parent, MemoizedFile dir)
 	{
 		super(rootNode, parent, dir);
-		seedLocator = new IndexPageSeedLocator(root());
-	}
-	
-	@Override
-	public MemoizedFile[] memoizedScopeFiles() {
-		List<MemoizedFile> scopeFiles = new ArrayList<>(Arrays.asList(app().memoizedScopeFiles()));
-		scopeFiles.add(dir());
-		
-		return scopeFiles.toArray(new MemoizedFile[0]);
-	}
-
-	public DirNode styleResources()
-	{
-		return styleResources.item();
 	}
 		
 	public Blade parent()
 	{
 		return (Blade) parentNode();
-	}
-	
-	@Override
-	public List<LinkedAsset> modelSeedAssets() {
-		return seedLocator.seedAssets(this);
-	}
-	
-	@Override
-	public void addTemplateTransformations(Map<String, String> transformations) throws ModelUpdateException
-	{
-	}
-	
-	@Override
-	public String requirePrefix() {
-		return app().getRequirePrefix();
-	}
-	
-	@Override
-	public boolean isNamespaceEnforced() {
-		return false;
 	}
 	
 	@Override
@@ -80,27 +36,5 @@ public final class BladeWorkbench extends AbstractBrowsableNode implements Testa
 		
 		return assetContainers;
 	}
-	
-	@Override
-	public void runTests(TestType... testTypes)
-	{
-		TestRunner.runTests(testTypes);
-	}
-	
-	@Override
-	public List<TypedTestPack> testTypes()
-	{
-		return testTypes.list();
-	}
-	
-	@Override
-	public TypedTestPack testType(String typedTestPackName)
-	{
-		return testTypes.item(typedTestPackName);
-	}
-	
-	@Override
-	public String getTypeName() {
-		return "workbench";
-	}
+
 }
---
+++ brjs-core/src/main/java/org/bladerunnerjs/model/Workbench.java
@@ -0,0 +1,84 @@
+package org.bladerunnerjs.model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.bladerunnerjs.memoization.MemoizedFile;
+import org.bladerunnerjs.model.engine.Node;
+import org.bladerunnerjs.model.engine.NodeItem;
+import org.bladerunnerjs.model.engine.NodeList;
+import org.bladerunnerjs.model.engine.RootNode;
+import org.bladerunnerjs.model.exception.modelupdate.ModelUpdateException;
+import org.bladerunnerjs.plugin.utility.IndexPageSeedLocator;
+import org.bladerunnerjs.utility.TestRunner;
+
+public abstract class Workbench extends AbstractBrowsableNode implements TestableNode
+{
+	private final NodeItem<DirNode> styleResources = new NodeItem<>(this, DirNode.class, "resources/style");
+	private final NodeList<TypedTestPack> testTypes = TypedTestPack.createNodeSet(this, TypedTestPack.class);
+	private final IndexPageSeedLocator seedLocator;
+	
+	public Workbench(RootNode rootNode, Node parent, MemoizedFile dir)
+	{
+		super(rootNode, parent, dir);
+		seedLocator = new IndexPageSeedLocator(root());
+	}
+	
+	@Override
+	public MemoizedFile[] memoizedScopeFiles() {
+		List<MemoizedFile> scopeFiles = new ArrayList<>(Arrays.asList(app().memoizedScopeFiles()));
+		scopeFiles.add(dir());
+		
+		return scopeFiles.toArray(new MemoizedFile[0]);
+	}
+
+	public DirNode styleResources()
+	{
+		return styleResources.item();
+	}
+	
+	@Override
+	public List<LinkedAsset> modelSeedAssets() {
+		return seedLocator.seedAssets(this);
+	}
+	
+	@Override
+	public void addTemplateTransformations(Map<String, String> transformations) throws ModelUpdateException
+	{
+	}
+	
+	@Override
+	public String requirePrefix() {
+		return app().getRequirePrefix();
+	}
+	
+	@Override
+	public boolean isNamespaceEnforced() {
+		return false;
+	}
+	
+	@Override
+	public void runTests(TestType... testTypes)
+	{
+		TestRunner.runTests(testTypes);
+	}
+	
+	@Override
+	public List<TypedTestPack> testTypes()
+	{
+		return testTypes.list();
+	}
+	
+	@Override
+	public TypedTestPack testType(String typedTestPackName)
+	{
+		return testTypes.item(typedTestPackName);
+	}
+	
+	@Override
+	public String getTypeName() {
+		return getClass().getSuperclass().getSimpleName();
+	}
+}
